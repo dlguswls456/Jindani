16cf949e056e4f19b24ae2c7add73ee005bb2cae
HYUNJIN LEE <dlguswls456@naver.com>
2021-08-03 00:32:11 +0900

    add google login

--- app/src/main/java/org/techtown/Jindani/LoginActivity.java
+++ app/src/main/java/org/techtown/Jindani/LoginActivity.java
@@ -1,14 +1,94 @@
 package org.techtown.Jindani;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private SignInButton btn_google;//구글 로그인 버튼
+
+    private FirebaseAuth auth;//파이어 베이스 인증 객체
+    private GoogleSignInClient googleSignInClient;//구글 API 클라이언트 객체
+    private static final int REQ_SIGN_GOOGLE = 100;//구글 로그인 결과 코드
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //구글 로그인
+        GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        googleSignInClient = GoogleSignIn.getClient(this, googleSignInOptions);
+
+        auth = FirebaseAuth.getInstance(); // 파이에베이스 인증 객체 초기화
+
+        btn_google = findViewById(R.id.btn_google);
+        btn_google.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = googleSignInClient.getSignInIntent();
+                startActivityForResult(intent, REQ_SIGN_GOOGLE);
+            }
+        });
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {// 구글 로그인 인증 요청 후
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQ_SIGN_GOOGLE) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            if (result.isSuccess()) {//인증 성공 여부 확인
+                GoogleSignInAccount account = result.getSignInAccount(); // account는 구글 로그인 정보 담고 있음(이메일 등등)
+                resultLogin(account);//로그인 결과값 출력 수행하라는 메소드
+            }
+        }
+    }
+
+    private void resultLogin(GoogleSignInAccount account) {//실제로 로그인 성공 했는지
+        AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
+        auth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){//로그인 성공
+                            Toast.makeText(LoginActivity.this, "로그인 성공", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(getApplicationContext(), SelectActivity.class);
+                            intent.putExtra("nickName", account.getDisplayName());//구글에 등록된 이름
+
+                            startActivity(intent);
+                        }else{//로그인 실패
+                            Toast.makeText(LoginActivity.this, "로그인 실페", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
 }
