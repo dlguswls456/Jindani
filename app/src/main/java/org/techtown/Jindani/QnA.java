0e64eaadacb0b518a899475e8b49d2ce95aa8940
JeongMin23 <0402rin@naver.com>
2021-08-03 11:16:27 +0900

    Create QnA.java

---
+++ app/src/main/java/org/techtown/Jindani/QnA.java
@@ -0,0 +1,28 @@
+package org.techtown.Jindani;
+
+public class QnA {
+
+    String qora; //질문 or 답변 내용
+    boolean isDoc; //발화자 구분
+
+    public QnA(String qora, boolean isDoc) {
+        this.qora = qora;
+        this.isDoc = isDoc;
+    }
+
+    public String getQora() {
+        return qora;
+    }
+
+    public void setQora(String qora) {
+        this.qora = qora;
+    }
+
+    public boolean isDoc() {
+        return isDoc;
+    }
+
+    public void setDoc(boolean doc) {
+        isDoc = doc;
+    }
+}
