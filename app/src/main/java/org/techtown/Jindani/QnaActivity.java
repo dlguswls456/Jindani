c0ce29a139965b615fe66f34cdabc14188319d80
JeongMin23 <0402rin@naver.com>
2021-08-03 11:16:33 +0900

    Create QnaActivity.java

---
+++ app/src/main/java/org/techtown/Jindani/QnaActivity.java
@@ -0,0 +1,39 @@
+package org.techtown.Jindani;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class QnaActivity extends AppCompatActivity {
+
+    RecyclerView qnalist;
+
+    private AdapterQnaList adapterQnaList;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.qna_list_page);
+
+        qnalist = findViewById(R.id.qnalist);
+        qnalist.setLayoutManager(new LinearLayoutManager(this));
+        adapterQnaList = new AdapterQnaList(qnalist);
+        qnalist.setAdapter(adapterQnaList);
+
+        adapterQnaList.addChatToList(new QnA("배가 아픈데 왜 그러나요?", false));
+        adapterQnaList.addChatToList(new QnA("배가 아픈데 왜 그러나요?", false));
+        adapterQnaList.addChatToList(new QnA("배가 아픈데 왜 그러나요?", false));
+        adapterQnaList.addChatToList(new QnA("배가 아픈데 왜 그러나요?", false));
+        adapterQnaList.addChatToList(new QnA("배가 아픈데 왜 그러나요?", false));
+        adapterQnaList.addChatToList(new QnA("배가 아픈데 왜 그러나요?", false));
+        adapterQnaList.addChatToList(new QnA("배가 아픈데 왜 그러나요?", false));
+        adapterQnaList.addChatToList(new QnA("배가 아픈데 왜 그러나요?", false));
+        adapterQnaList.addChatToList(new QnA("배가 아픈데 왜 그러나요?", false));
+        adapterQnaList.addChatToList(new QnA("배가 아픈데 왜 그러나요?", false));
+
+    }
+
+}
