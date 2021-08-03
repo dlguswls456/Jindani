d6ece502dd343dd6e2957822722c6911c837fd24
HYUNJIN LEE <dlguswls456@naver.com>
2021-08-03 15:28:49 +0900

    update google login
    로그인 수정 및 로그아웃 추가

--- app/src/main/java/org/techtown/Jindani/LoginActivity.java
+++ app/src/main/java/org/techtown/Jindani/LoginActivity.java
@@ -6,7 +6,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.gms.auth.api.Auth;
@@ -17,12 +19,14 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
 
 public class LoginActivity extends AppCompatActivity {
@@ -31,7 +35,8 @@ public class LoginActivity extends AppCompatActivity {
 
     private FirebaseAuth auth;//파이어 베이스 인증 객체
     private GoogleSignInClient googleSignInClient;//구글 API 클라이언트 객체
-    private static final int REQ_SIGN_GOOGLE = 100;//구글 로그인 결과 코드
+    private static final int RC_SIGN_IN = 100;//로그인 결과 코드
+    private String TAG = "LoginTag";
 
 
     @Override
@@ -53,43 +58,111 @@ public class LoginActivity extends AppCompatActivity {
         btn_google.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = googleSignInClient.getSignInIntent();
-                startActivityForResult(intent, REQ_SIGN_GOOGLE);
+                signIn();
             }
         });
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {// 구글 로그인 인증 요청 후
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = auth.getCurrentUser();
+        //updateUI(currentUser);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if (requestCode == REQ_SIGN_GOOGLE) {
-            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
-            if (result.isSuccess()) {//인증 성공 여부 확인
-                GoogleSignInAccount account = result.getSignInAccount(); // account는 구글 로그인 정보 담고 있음(이메일 등등)
-                resultLogin(account);//로그인 결과값 출력 수행하라는 메소드
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {//인증 성공 여부 확인
+                // Google Sign In was successful, authenticate with Firebase
+                // account는 구글 로그인 정보 담고 있음(이메일 등등)
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account);
+            } catch (ApiException e) {
+                // Google Sign In failed, update UI appropriately
+                Log.w(TAG, "Google sign in failed", e);
+                Toast.makeText(getApplicationContext(), "Google sign in Failed", Toast.LENGTH_SHORT).show();
             }
         }
     }
 
-    private void resultLogin(GoogleSignInAccount account) {//실제로 로그인 성공 했는지
+    private void firebaseAuthWithGoogle(GoogleSignInAccount account) {//실제로 로그인 성공했는지
+        Log.d(TAG, "firebaseAuthWithGoogle:" + account.getId());
+        // [START_EXCLUDE silent]
+        //showProgressDialog();
+        // [END_EXCLUDE]
+
         AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
         auth.signInWithCredential(credential)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()){//로그인 성공
-                            Toast.makeText(LoginActivity.this, "로그인 성공", Toast.LENGTH_SHORT).show();
-                            Intent intent = new Intent(getApplicationContext(), SelectActivity.class);
-                            intent.putExtra("nickName", account.getDisplayName());//구글에 등록된 이름
+                        if (task.isSuccessful()) {//로그인 성공
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+                            FirebaseUser user = auth.getCurrentUser();
+                            //updateUI(user);
+                            Toast.makeText(getApplicationContext(), account.getDisplayName() + " 로그인 성공", Toast.LENGTH_SHORT).show();
 
+                            Intent intent = new Intent(LoginActivity.this, SelectActivity.class);
                             startActivity(intent);
                             finish();
-                        }else{//로그인 실패
-                            Toast.makeText(LoginActivity.this, "로그인 실패", Toast.LENGTH_SHORT).show();
+                        } else {//로그인 실패
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            Toast.makeText(getApplicationContext(), "로그인 실패", Toast.LENGTH_SHORT).show();
+
+                            // updateUI(null);
                         }
+                        // [START_EXCLUDE]
+                        // hideProgressDialog();
+                        // [END_EXCLUDE]
                     }
                 });
+    }
+
+    //로그인
+    private void signIn() {
+        Intent signInIntent = googleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    //로그아웃
+    private void signOut() {
+        // Firebase sign out
+        auth.signOut();
+
+        // Google sign out
+        googleSignInClient.signOut().addOnCompleteListener(this,
+                new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        Toast.makeText(getApplicationContext(), "로그아웃 성공", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    private void revokeAccess() {
+        // Firebase sign out
+        auth.signOut();
+
+        // Google revoke access
+        googleSignInClient.revokeAccess().addOnCompleteListener(this,
+                new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        Toast.makeText(getApplicationContext(), "Complete", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    private void updateUI(FirebaseUser user) {
+
     }
 
 }
