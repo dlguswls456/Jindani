c94936b648219ba30fdc384d317fff6ae064ef79
JeongMin23 <0402rin@naver.com>
2021-08-11 01:49:56 +0900

    Search question

--- app/src/main/java/org/techtown/Jindani/adapter/AdapterQnaList.java
+++ app/src/main/java/org/techtown/Jindani/adapter/AdapterQnaList.java
@@ -3,6 +3,8 @@ package org.techtown.Jindani.adapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -15,7 +17,8 @@ import java.util.ArrayList;
 
 public class AdapterQnaList extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
-    public ArrayList<QnaModel> list = new ArrayList<QnaModel>();
+    public ArrayList<QnaModel> list = new ArrayList<>();
+    public ArrayList<QnaModel> copyList = new ArrayList<>();
     private RecyclerView qnaList;
 
     public AdapterQnaList(RecyclerView qnalist) {
@@ -34,16 +37,53 @@ public class AdapterQnaList extends RecyclerView.Adapter<RecyclerView.ViewHolder
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-        ((AdapterQnaList.ViewHolder) holder).bind(this.list.get(position));
+        ((AdapterQnaList.ViewHolder) holder).bind(list.get(position));
     }
 
     @Override
     public int getItemCount() {
-        return this.list.size();
+        return list.size();
     }
+
+    public Filter getFilter(){
+        return exampleFilter;
+    }
+
+    private Filter exampleFilter = new Filter() {
+        @Override
+        protected FilterResults performFiltering(CharSequence constraint) {
+
+            ArrayList<QnaModel> filteredList = new ArrayList<>();
+
+            if(constraint == null || constraint.length() == 0){
+                filteredList.addAll(copyList);
+            }else{
+                String filterPattern = constraint.toString().toLowerCase().trim();
+
+                for(QnaModel item : copyList){
+                    if(item.getQuestion_title().toLowerCase().contains(filterPattern) ||
+                            item.getQuestion_content().toLowerCase().contains(filterPattern)){
+                        filteredList.add(item);
+                    }
+                }
+            }
+            FilterResults results = new FilterResults();
+            results.values = filteredList;
+
+            return results;
+        }
+
+        @Override
+        protected void publishResults(CharSequence constraint, FilterResults results) {
+            list.clear();
+            list.addAll((ArrayList)results.values);
+            notifyDataSetChanged();
+        }
+    };
 
     public void addQToList(QnaModel qna){
         this.list.add(qna);
+        this.copyList.add(qna);
         this.notifyDataSetChanged();
         //채팅 추가될때마다 리사이클러뷰가 제일 하단으로 가게함
         qnaList.scrollToPosition(getItemCount()-1);
